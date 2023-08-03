563605f582096b91de82b14d5ec0a137ce484a57
Wallekson <walisoncastor@gmail.com>
2023-08-03 20:22:15 -0300

    Exercicio Nome Completo

---
+++ script1.js
@@ -0,0 +1,8 @@
+let rlSync = require('readline-sync')
+let nome = rlSync.question("Qual o seu nome?\n")
+
+let sobrenome = rlSync.question("Qual o seu sobrenome?\n")
+
+let nomeCompleto = nome + " " + sobrenome
+
+console.log(`Seu nome completo é: ${nomeCompleto}`)
